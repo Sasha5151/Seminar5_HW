0219ff1b107df6344ff3ccce6830d12081a7a838
Sasha <F712@rambler.ru>
2022-09-15 10:39:39 +0300

    Задача 38

--- Program.cs
+++ Program.cs
@@ -38,35 +38,31 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int size = 10;
-int[]array = new int[size];
-FillArray (array);
-System.Console.WriteLine("[{0}]" , string.Join(" , ", array));
-Sum(array);
+// int size = 10;
+// int[]array = new int[size];
+// FillArray (array);
+// System.Console.WriteLine("[{0}]" , string.Join(" , ", array));
+// Sum(array);
 
-void FillArray (int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-    array[i] = new Random().Next(-100 , 100);
-    }
-}
+// void FillArray (int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//     array[i] = new Random().Next(-100 , 100);
+//     }
+// }
 
-void Sum(int [] array)
-{
-  int Sum = 0 ;
-    for (int i = 1; i < array.Length; i=i+2)
-    {
-     Sum = Sum + array[i] ;
-    }
-     System.Console.WriteLine(Sum);
-}
+// void Sum(int [] array)
+// {
+//   int Sum = 0 ;
+//     for (int i = 1; i < array.Length; i=i+2)
+//     {
+//      Sum = Sum + array[i] ;
+//     }
+//      System.Console.WriteLine(Sum);
+// }
 
 //End
-
-
-
-
 
 
 
@@ -74,3 +70,31 @@ void Sum(int [] array)
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
+int size = 15;
+int[]array = new int[size];
+FillArray (array);
+System.Console.WriteLine("[{0}]" , string.Join(" , ", array));
+MaxMin(array);
+
+void FillArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = new Random().Next(0 , 100);
+    }
+}
+
+void MaxMin(int [] array )
+{
+     int max = array[0];
+     int min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        max = array[i];
+        if (array[i] < min)
+        min = array[i];
+    }
+    int Delta = max - min ;
+    System.Console.WriteLine(Delta);
+}
