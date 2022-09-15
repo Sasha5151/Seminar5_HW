9d09e658b04f595c125e6f3b7be0976dd144ec7d
Sasha <F712@rambler.ru>
2022-09-15 10:21:48 +0300

    Задача 36

--- Program.cs
+++ Program.cs
@@ -33,13 +33,42 @@
 
 
 
-
-
 // Задача 36: Задайте одномерный массив, заполненный случайными числами.
 //  Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
-
 // [-4, -6, 89, 6] -> 0
+
+int size = 10;
+int[]array = new int[size];
+FillArray (array);
+System.Console.WriteLine("[{0}]" , string.Join(" , ", array));
+Sum(array);
+
+void FillArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = new Random().Next(-100 , 100);
+    }
+}
+
+void Sum(int [] array)
+{
+  int Sum = 0 ;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+     Sum = Sum + array[i] ;
+    }
+     System.Console.WriteLine(Sum);
+}
+
+//End
+
+
+
+
+
+
 
 // Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
